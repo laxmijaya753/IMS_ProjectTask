d583c135b268aa902f454982a81aac79e7df3a36
laxmijaya753 <you@example.com>
2021-06-04 00:43:15 -0400

    Jaya:Changed Pom.xml file

---
+++ src/main/java/com/qa/ims/persistence/dao/Dao.java
@@ -0,0 +1,24 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.qa.ims.persistence.domain.Customer;
+
+public interface Dao<T> {
+
+	List<T> readAll();
+	
+	T read(Long id);
+
+	T create(T t);
+
+	T update(T t);
+
+	int delete(long id);
+
+	T modelFromResultSet(ResultSet resultSet) throws SQLException;
+
+	
+}
