3c9a7cbc077d85485a824ee87f4ef88b68071d8a
laxmijaya753 <you@example.com>
2021-06-04 15:03:19 -0400

    Jaya:Moving files to github dev

---
+++ src/main/java/com/qa/ims/IMS.java
@@ -0,0 +1,120 @@
+package com.qa.ims;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.controller.Action;
+import com.qa.ims.controller.CrudController;
+import com.qa.ims.controller.CustomerController;
+import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.domain.Domain;
+import com.qa.ims.utils.DBUtils;
+import com.qa.ims.utils.Utils;
+
+import com.qa.ims.controller.OrderController;
+import com.qa.ims.persistence.dao.OrderDAO;
+
+import com.qa.ims.controller.ItemController;
+import com.qa.ims.persistence.dao.ItemDAO;
+
+public class IMS {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private final CustomerController customers;
+	//Added code for Items
+		private final ItemController items;
+		//Added code for order
+		private final OrderController orders;
+	
+	private final Utils utils;
+
+	public IMS() {
+		this.utils = new Utils();
+		final CustomerDAO custDAO = new CustomerDAO();
+		this.customers = new CustomerController(custDAO, utils);
+		
+		
+		//Added code for Items
+				final ItemDAO itemDAO=new ItemDAO();
+				this.items = new ItemController(itemDAO, utils);
+				
+				//Added code for order
+				
+				
+				final OrderDAO orderDAO=new OrderDAO();
+				this.orders = new OrderController(orderDAO, utils);
+	}
+
+	public void imsSystem() {
+		LOGGER.info("Welcome to the Inventory Management System!");
+		DBUtils.connect();
+
+		Domain domain = null;
+		do {
+			LOGGER.info("Which entity would you like to use?");
+			Domain.printDomains();
+
+			domain = Domain.getDomain(utils);
+
+			domainAction(domain);
+
+		} while (domain != Domain.STOP);
+	}
+
+	private void domainAction(Domain domain) {
+		boolean changeDomain = false;
+		do {
+
+			CrudController<?> active = null;
+			switch (domain) {
+			case CUSTOMER:
+				active = this.customers;
+				break;
+			case ITEM:
+				active = this.items;
+				break;
+			case ORDER:
+				active = this.orders;
+				break;
+			case STOP:
+				return;
+			default:
+				break;
+			}
+
+			LOGGER.info(() ->"What would you like to do with " + domain.name().toLowerCase() + ":");
+
+			Action.printActions();
+			Action action = Action.getAction(utils);
+
+			if (action == Action.RETURN) {
+				changeDomain = true;
+			} else {
+				doAction(active, action);
+			}
+		} while (!changeDomain);
+	}
+
+	public void doAction(CrudController<?> crudController, Action action) {
+		switch (action) {
+		case CREATE:
+			crudController.create();
+			break;
+		case READ:
+			crudController.readAll();
+			break;
+		case UPDATE:
+			crudController.update();
+			break;
+		case DELETE:
+			crudController.delete();
+			break;
+		case RETURN:
+			break;
+		default:
+			break;
+		}
+	}
+
+}
---
+++ src/main/resources/sql-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('?', '?');
+
+
+INSERT INTO  `ims`.`item` (item_id,name,value1) VALUES ('?','?','?');
+
+INSERT INTO  `ims`.`order_items` (customer_id,item_id1,quantity) VALUES ('?','?','?');
+
+INSERT INTO  `ims`.`orders` (customer_id1) VALUES ('?');
